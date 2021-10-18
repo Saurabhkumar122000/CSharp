8850a799764b1c5621ecce205d7d6ec2a65ba5d5
Saurabhkumar122000 <92707035+Saurabhkumar122000@users.noreply.github.com>
2021-10-18 12:42:47 +0530

    Add files via upload

---
+++ FirstProgram/FirstProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is our first programe");
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+        }
+    }
+}
